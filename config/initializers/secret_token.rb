c9a78a215c34172ff98b5506f08acf86fc4bb9b0
ahwee1987 <ahwee1987@gmail.com>
2011-10-18 23:51:42 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tagloop::Application.config.secret_token = '413c64f0b24bd0c296a83e0362b26465c58b55297cadb098272ba8d78a15c605be0da43654bb751ed31c8ba6e0bb99ca0fba9888ae4440ba0c70fcc6b4b19c71'
